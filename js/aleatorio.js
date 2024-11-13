796f2cef8dd1742e80912f1f7b51590d3330415d
Vinicius-lara <joao.lara.martins@escola.pr.gov.br>
2024-11-13 15:47:25 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Vinícius Lara", "Gustavo Sarote", "Bruno Souza", "Davi Lucas", "Victor", "Rafael Rotulo", "Joabe"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
